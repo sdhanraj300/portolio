d5720fff96c73f96ed6d1f5d6ee1f2aa55c2af39
Dhanraj Singh <sdhanraj300@gmail.com>
2024-11-10 10:16:30 +0530

    did it2

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from 'next/image';
 import React from 'react';
-import img from '../assets/coder.avif';
+import img from '../assets/coder.png';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 
