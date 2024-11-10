d275806b47cf91b79a7fe2ae6ef7befc0bd76503
Dhanraj Singh <sdhanraj300@gmail.com>
2024-11-10 10:25:19 +0530

    did it3

--- src/app/projects/[...id]/page.tsx
+++ src/app/projects/[...id]/page.tsx
@@ -9,13 +9,14 @@ import img4 from '@/assets/project-4.png';
 
 const ProjectPage = () => {
   const params = useParams();
-  const title = params['id'][0];
+  const title = params['id'] ? params['id'][0] : null;
+
   const projects = {
     serenity: {
       img: img1,
       title: 'Serenity Events',
       description:
-        'Serenity is an intuitive event management app that simplifies the process of creating, managing, and attending events.Users can create events, invite guests(both registered and non- registered), and track RSVP responses in real- time.Event creators have the ability to edit guest lists before sending invitations and monitor attendance status.Attendees can interact by posting images, GIFs, emojis, and comments on the event page.Serenity email invitations are sent through SendGrid, ensuring a seamless and engaging experience for both hosts and guests.',
+        'Serenity is an intuitive event management app that simplifies the process of creating, managing, and attending events. Users can create events, invite guests (both registered and non-registered), and track RSVP responses in real-time. Event creators have the ability to edit guest lists before sending invitations and monitor attendance status. Attendees can interact by posting images, GIFs, emojis, and comments on the event page. Serenity email invitations are sent through SendGrid, ensuring a seamless and engaging experience for both hosts and guests.',
       technologies: ['React', 'Next.js', 'Node.js', 'Tailwind CSS', 'Prisma', 'SendGrid'],
       projectLink: 'https://serenityevents.vercel.app/',
       githubLink: 'https://github.com/sdhanraj300/serenity-events',
@@ -34,7 +35,7 @@ const ProjectPage = () => {
       title: 'Mindful AI',
       description:
         'MindfulAI is a chatbot app designed to provide students with empathetic support for mental health-related issues. Using the Gemini API for its language model, MindfulAI offers personalized, compassionate responses to users questions, helping them navigate mental challenges. The chatbot is custom-trained with a wide range of questions and answers, ensuring it responds in an empathetic and understanding manner. Built with React, Redux, MongoDB, and Prisma, MindfulAI aims to create a safe and supportive environment for students seeking guidance on mental health concerns.',
-      technologies: ['React', 'Node.js', 'MongoDB', 'Express', 'Gemini API','GenAI'],
+      technologies: ['React', 'Node.js', 'MongoDB', 'Express', 'Gemini API', 'GenAI'],
       projectLink: 'https://mindfulai.vercel.app/',
       githubLink: 'https://github.com/sdhanraj300/genai-project',
     },
@@ -43,7 +44,7 @@ const ProjectPage = () => {
       title: 'Metaverse',
       description:
         'Metaverse is an immersive front-end application that showcases advanced web development skills through interactive 3D environments. Built with Framer Motion, it brings a dynamic and fluid experience to the web, featuring smooth animations, transitions, and engaging UI elements. The app leverages Framer Motion’s powerful animation library to create seamless, high-performance motion effects, enhancing the user experience as they explore the virtual space. Metaverse demonstrates the capabilities of modern web technologies to create an interactive, visually compelling front-end that merges design with functionality, perfect for showcasing creative and technical front-end development expertise.',
-      technologies: ['React', 'Next.js', 'Node.js', 'Tailwind CSS','Framer Motion'],
+      technologies: ['React', 'Next.js', 'Node.js', 'Tailwind CSS', 'Framer Motion'],
       projectLink: 'https://metaverse-69.netlify.app/',
       githubLink: 'https://github.com/sdhanraj300/Metaversee',
     },
